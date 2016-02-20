c5f22e4c589d53fdc9e209932b9057cdc773d666
Amit Rajawat <rajawat@blackbuck.com>
2016-02-21 01:33:57 +0530

    Algo ready to deploy

--- googleMapAPI/config.py
+++ googleMapAPI/config.py
@@ -3,3 +3,18 @@ GOOGLE_KEY = "AIzaSyAAsVfV-jYZNI5NnPmE22VmDfL6dH8_vuw"
 FAVORITE_KEYWORDS = {"sports" : ['sport center','cricket', 'swimming pool'],
                      "bar" : ['pub','club', 'bar','']
                      }
+
+STATUS_DICT = {
+'single':
+			[
+				{'name':'Office','value':100},
+				{'name':'Sports','value':70},
+				{'name':'Pub','value':30}
+			],
+'married':
+			[
+				{'name':'Hospital','value':80},
+			 	{'name':'Mall','value':30},
+			 	{'name':'School','value':50}
+			]
+}
